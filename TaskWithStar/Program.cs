b0dda9255950409db80f7fd269898f544987a724
[Ruliaga] <ruliaga@mail.ru>
2022-08-31 16:11:35 +0500

    TaskWithStar done

---
+++ TaskWithStar/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Clear();
+Console.WriteLine("Нажмите Enter для вывода массива.");
+Console.ReadLine();
+
+int[] array = new int[8];
+Random rnd=new Random();
+
+Console.Write("[");
+int max=0;
+
+for(int i=0;i<array.Length;i++){
+
+array[i]=rnd.Next(-10,10);
+
+
+Console.Write(array[i]);
+
+if(max<array[i]){max=array[i];}
+
+
+if(i<array.Length-1){Console.Write(", ");}
+else {Console.Write("] -> ");}
+
+}
+Console.Write(max);
