dfed33bab4dfd41b29c866f35e8cf22eec1e2822
[Ruliaga] <ruliaga@mail.ru>
2022-08-31 14:29:55 +0500

    Task27 done

---
+++ Task27/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число:");
+string number=Convert.ToString(Console.ReadLine());
+
+int sum=0;
+
+for(int i=0;i<number.Length;i++){
+
+    sum=sum+Convert.ToInt32(number[i].ToString());
+}
+Console.WriteLine($"{number} -> {sum}");
