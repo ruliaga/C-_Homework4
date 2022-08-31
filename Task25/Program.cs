73ee9bc7974eecd7cca57170a9bc0afda6e5ebc8
[Ruliaga] <ruliaga@mail.ru>
2022-08-31 13:14:56 +0500

    Task25 done

---
+++ Task25/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.WriteLine("Введите первое число:");
+double number1=Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите второе число:");
+double number2=Convert.ToDouble(Console.ReadLine());
+
+int result=Convert.ToInt32(Math.Pow(number1,number2));
+
+Console.WriteLine($"{number1}, {number2} -> {result}");
